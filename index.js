0ed14bb013b6846341433b3e38a4b9d51ccbb596
gusss666 <gustavo.costa3@alunos.unis.edu.br>
2023-06-25 19:42:56 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+function irLogin(){
+    window.location = "login.html"
+}
+function irCadastro(){
+    window.location = "Cadastrar.html"
+}
+function irPaginaInicial(){
+    window.location = "Pagina1.html"
+}
+function irProduto1(){
+    window.location = "Produto1.html"
+}
+function irProduto2(){
+    window.location = "Produto2.html"
+}
+function irProduto3(){
+    window.location = "Produto3.html"
+}
+function irProduto4(){
+    window.location = "Produto4.html"
+}
+function irProduto7(){
+    window.location = "Produto7.html"
+}
+function irProduto8(){
+    window.location = "Produto8.html"
+}
+function irCarrinho1(){
+    window.location = "Carrinho1.html"
+}
